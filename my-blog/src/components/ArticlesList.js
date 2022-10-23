15ebcb2ed8833b519a235655ba75869a7c42ca3c
ccasti26 <ccasti26@calstatela.edu>
2022-10-23 16:16:13 -0700

    implemented Article List Page

---
+++ my-blog/src/components/ArticlesList.js
@@ -0,0 +1,19 @@
+import articles from "../pages/article-content"
+import { Link } from "react-router-dom";
+
+const ArticlesList = () => {
+    return (
+        <>
+        {articles.map(article => (
+            
+            <Link key={article.name} to={`/articles/${article.name}`} className="article-list-item">
+                <h3>{article.title}</h3>
+                <p>{article.content[0].substring(0, 150)}...</p>
+            </Link>
+        ))}
+        </>
+        
+    )
+}
+
+export default ArticlesList;
