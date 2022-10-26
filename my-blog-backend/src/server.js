e1ec06f9f5d2cf0820affcd867413a00c05929df
ccasti26 <ccasti26@calstatela.edu>
2022-10-25 23:53:05 -0700

    Implemented Comments to articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,12 +3,15 @@ import express from 'express';
 let articlesInfo = [{
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
 },{ 
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
 },{ 
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
 }]
 
 const app = express();
@@ -23,7 +26,21 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     } else {
         res.send('That article doesn\'t exist');
     }
-})
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const {postedBy, text} = req.body;
+    const {name} = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+
+    if(article){
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+    }else {
+        res.send('That article doesn\'t exist');
+    }
+
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
