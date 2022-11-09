90549958ae4ffd62062e6bc4c8b7abaec9336450
ccasti26 <ccasti26@calstatela.edu>
2022-11-08 16:25:45 -0800

    Added Firebase Auth to project

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -5,6 +5,8 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
+import LoginPage from './pages/LoginPage';
+import CreateAccountPage from './pages/CreateAccountPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
@@ -14,11 +16,13 @@ function App() {
         <NavBar/>
         <div id="page-body">
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/articles" element={<ArticlesListPage />} />
-            <Route path="/articles/:articleId" element={<ArticlePage />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route  path="/"                      element={<HomePage />} />
+            <Route  path="/about"                 element={<AboutPage />} />
+            <Route  path="/articles"              element={<ArticlesListPage />} />
+            <Route  path="/articles/:articleId"   element={<ArticlePage />} />
+            <Route  path='/login'                 element={<LoginPage />} />
+            <Route  path='create-account'         element={<CreateAccountPage/>} />
+            <Route  path="*"                      element={<NotFoundPage />} />
           </Routes>
         </div>
       </div>
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWEKWwyMNw-UA625yjZd9NcotUVk382dY",
+  authDomain: "my-react-blog-22bbf.firebaseapp.com",
+  projectId: "my-react-blog-22bbf",
+  storageBucket: "my-react-blog-22bbf.appspot.com",
+  messagingSenderId: "877880797442",
+  appId: "1:877880797442:web:7ecdeef8f8b3c3b2bb3a1b"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
