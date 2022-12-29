ea8820f7cf8605cce0510d174aaba01839219952
ccasti26 <ccasti26@calstatela.edu>
2022-12-28 18:07:21 -0800

    App ready for launch

--- my-blog/src/Navbar.js
+++ my-blog/src/Navbar.js
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link,  useNavigate } from "react-router-dom";
+import { getAuth, signOut } from 'firebase/auth';
+import useUser from "./hooks/useUser";
 
 const NavBar = () => {
+    const { user } = useUser();
+    const navigate = useNavigate();
+
     return (
         <nav>
             <ul>
@@ -13,8 +18,14 @@ const NavBar = () => {
                 <li>
                     <Link to="/articles">Articles</Link>
                 </li>
-
             </ul>
+
+            <div className="nav-right">
+                { user 
+                    ? <button onClick={() => {signOut(getAuth());}}>Log Out</button>
+                    : <button onClick={() => {navigate('/login')}}>Log In</button>}
+                
+            </div>
         </nav>
 
     );
